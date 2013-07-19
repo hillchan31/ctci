2bbb4f32494df96274a4f1f75d2596d232e43542
hillchan31 <hillchan31@gmail.com>
2013-07-19 16:05:28 +0800

    q2.4

--- src/q2_4.java
+++ src/q2_4.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author hill
@@ -8,49 +9,23 @@ public class q2_4 {
     }
 
     public static void main(String args[]) {
-        LinkedListNode aNode = null;
-        LinkedListNode bNode = null;
-        LinkedListNode tmp = null;
         // a = 815
         int a[] = {5, 1, 8,};
         // b = 3186
         int b[] = {6, 8, 1, 3};
+        LinkedListNode aNode = arrToList(a);
+        LinkedListNode bNode = arrToList(b);
 
-        for (int i = 0; i < a.length; i++) {
-            LinkedListNode next = new LinkedListNode(a[i]);
-            if (i == 0) {
-                aNode = tmp = next;
-                continue;
-            }
-            tmp.next = next;
-            tmp = next;
-        }
-
-        for (int i = 0; i < b.length; i++) {
-            LinkedListNode next = new LinkedListNode(b[i]);
-            if (i == 0) {
-                bNode = tmp = next;
-                continue;
-            }
-            tmp.next = next;
-            tmp = next;
-        }
-
+        //output aNode: 518
         printList(aNode);
+        //output bNode 6813
         printList(bNode);
 
-//        // Print the list(before) 
-//        tmp = aNode;
-//        while (tmp != null) {
-//            System.out.println(tmp.data);
-//            tmp = tmp.next;
-//        };
-//
-//        tmp = bNode;
-//        while (tmp != null) {
-//            System.out.println(tmp.data);
-//            tmp = tmp.next;
-//        };
+        LinkedListNode result = add(aNode, bNode, 0);
+
+        //output result 1004
+        printList(result);
+
     }
 
     public static void printList(LinkedListNode a) {
@@ -59,12 +34,14 @@ public class q2_4 {
             System.out.println(tmp.data);
             tmp = tmp.next;
         };
+        System.out.println("");
     }
 
-    public final static void arrToList(int[] arr, LinkedListNode node) {
+    public static LinkedListNode arrToList(int[] a) {
+        LinkedListNode node = null;
         LinkedListNode tmp = null;
-        for (int i = 0; i < arr.length; i++) {
-            LinkedListNode next = new LinkedListNode(arr[i]);
+        for (int i = 0; i < a.length; i++) {
+            LinkedListNode next = new LinkedListNode(a[i]);
             if (i == 0) {
                 node = tmp = next;
                 continue;
@@ -72,10 +49,28 @@ public class q2_4 {
             tmp.next = next;
             tmp = next;
         }
+        return node;
     }
 
     public static LinkedListNode add(LinkedListNode a, LinkedListNode b, int carry) {
+        if (a == null && b == null) {
+            return null;
+        }
 
-        return null;
+        LinkedListNode result = new LinkedListNode(carry);
+        int value = carry;
+        if (a != null) {
+            value += a.data;
+        }
+        if (b != null) {
+            value += b.data;
+        }
+
+        result.data = value % 10;
+
+        LinkedListNode more = add(a == null ? null : a.next, b == null ? null : b.next, value > 9 ? 1 : 0);
+
+        result.next = more;
+        return result;
     }
 }
